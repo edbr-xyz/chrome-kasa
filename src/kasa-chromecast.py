6a1af61a1792335beacd7a99399ff6b869e41792
edbr-xyz <git@edbr.xyz>
2024-12-06 23:47:59 +0000

    init

---
+++ src/kasa-chromecast.py
@@ -0,0 +1,28 @@
+import time
+import subprocess
+import sys
+def main():
+    chromecast = sys.argv[1].replace('###space###', ' ')
+    kasaPlug = sys.argv[2]
+    print('Controlling Kasa plug at', kasaPlug, 'based on the status of', chromecast)
+    sleepTime = idle = 60
+    ampStatus = "unknown"
+    while True:
+        if str.encode('"appType\\":\\"WEB\\"') in subprocess.Popen(['go-chromecast','status', '-n', chromecast, '-v'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]:
+            idle = 0
+            if ampStatus != "on":
+                subprocess.run(["kasa", "--type", "plug", "--host", kasaPlug, "on"])
+                ampStatus = "on"
+        elif sleepTime > idle:
+            idle += 1
+        elif ampStatus != "off":
+            subprocess.run(["kasa", "--type", "plug", "--host", kasaPlug, "off"])
+            ampStatus = "off"
+
+        time.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
